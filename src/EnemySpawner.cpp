ae13673c74ab445c613840f013e9aab1de5d8691
Vladimir Malinovskii <galqiwi@galqiwi.ru>
2021-04-30 04:25:39 +0300

    upd

--- src/EnemySpawner.cpp
+++ src/EnemySpawner.cpp
@@ -6,24 +6,22 @@
 
 class Enemy : public Entity {
 public:
-    using Entity::Entity;
-    void update() override;
+    Enemy(double size, Vector2D position, size_t texture_id, size_t health):
+            Entity(ENEMY_PIXEL_SIZE, position_, ENEMY_TEXTURE_ID), health_(health) {
+    }
+    void update() {
+        // TODO: enemy ai
+    }
 private:
     uint32_t health_;
-    friend EnemySpawner;
 };
 
-void Enemy::update() {
-    //TODO: Enemy AI
-}
 
 EnemySpawner::EnemySpawner(Scene *scene, Vector2D position):
         EntitySpawner(scene), position_(position){
-
 }
 
 Entity* EnemySpawner::create() {
-    auto* out = new Enemy(ENEMY_PIXEL_SIZE, position_, ENEMY_TEXTURE_ID);
-    out->health_ = ENEMY_DEFAULT_HEALTH;
+    auto* out = new Enemy(ENEMY_PIXEL_SIZE, position_, ENEMY_TEXTURE_ID, ENEMY_DEFAULT_HEALTH);
     return out;
 }
